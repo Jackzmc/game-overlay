b50876d39eeeb71ae8800cb6cf6a701cc7e75757
Jackz <me@jackz.me>
2024-04-30 13:03:58 -0500

    add logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,15 @@
 
 mod manager;
 
+use log::debug;
+use std::env;
 use sysinfo::{Pid, ProcessStatus, System};
 use tauri::{AppHandle, Manager, PhysicalPosition, Position, Size, State, Url, Window};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use active_win_pos_rs::get_active_window;
 use tungstenite::{connect, Message};
+use log::{error, trace};
 use crate::manager::ManagerResponse;
 
 
@@ -46,6 +49,11 @@ impl AppData {
 }
 
 fn main() {
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", format!("warn,{}=info", env!("CARGO_PKG_NAME")));
+    }
+    pretty_env_logger::init();
+
     let context = tauri::generate_context!();
     let data = Mutex::new(AppData::new());
     tauri::Builder::default()
@@ -65,7 +73,7 @@ fn main() {
             Ok(())
         })
         .manage(data)
-        .invoke_handler(tauri::generate_handler![check_process, init_manager, init_login, overlay_key, init_process_check])
+        .invoke_handler(tauri::generate_handler![init_manager, init_login, overlay_key, init_process_check])
         .run(context)
         .expect("error while running tauri application");
 }
@@ -85,19 +93,28 @@ enum ProcessData {
 #[tauri::command]
 fn init_process_check(window: tauri::Window) {
     std::thread::spawn(move || {
+        debug!("process check thread started");
         loop {
             let state = window.state::<Mutex<AppData>>();
             let mut data = state.lock().unwrap();
             data.sys.refresh_processes();
             // let active_pid = get_active_window_pid();
-            let active_window = get_active_window().unwrap();
+            let active_window = match get_active_window() {
+                Ok(window) => window,
+                Err(_) => {
+                    error!("get_active_window returned error");
+                    continue;
+                }
+            };
             let our_pid = std::process::id() as u64;
+            trace!("our_pid={our_pid} active_window pid={}", active_window.process_id);
             let mut active = data.view_state == ViewState::Visible;
             // println!("active={} target={}", active_window.app_name, TARGET_PROC_NAME);
             if let Some(proc) = data.sys.processes_by_name(TARGET_PROC_NAME).next() {
                 let pid = proc.pid().as_u32();
                 // println!("proc found. pid={} active_pid={}", pid, active_window.process_id);
                 if active_window.process_id == our_pid || active_window.process_id == pid as u64 || active_window.app_name == TARGET_PROC_NAME {
+                    trace!("found proc. pid={pid}");
                     window.emit("process", ProcessDataResult {
                         pid,
                         cpu_usage: proc.cpu_usage(),
@@ -119,54 +136,16 @@ fn init_process_check(window: tauri::Window) {
                     data.view_state = ViewState::Hidden;
                     // window.hide().unwrap();
                     window.close().unwrap();
+                    debug!("app is now inactive, hiding overlay");
                 } else if data.view_state == ViewState::Hidden && active {
                     data.view_state = ViewState::Visible;
                     window.show().unwrap();
+                    debug!("app is now active, showing overlay");
                 }
             }
             std::thread::sleep(Duration::from_millis(PROCESS_CHECK_INTERVAL));
         }
     });
-}
-#[tauri::command]
-fn check_process(app: tauri::AppHandle, data: State<Mutex<AppData>>) -> Option<ProcessDataResult> {
-    let mut data = data.lock().unwrap();
-    data.sys.refresh_processes();
-    let window = app.get_window("main").unwrap();
-    // let active_pid = get_active_window_pid();
-    let active_window = get_active_window().unwrap();
-    println!("active = {:?}", active_window);
-    let our_pid = std::process::id() as u64;
-    let mut active = data.view_state == ViewState::Visible;
-    let mut result: Option<ProcessDataResult> = None;
-    if let Some(proc) = data.sys.processes_by_exact_name(TARGET_PROC_NAME).next() {
-        let pid = proc.pid().as_u32();
-        if active_window.process_id == our_pid || active_window.process_id == pid as u64{
-            result = Some(ProcessDataResult {
-                pid: proc.pid().as_u32(),
-                cpu_usage: proc.cpu_usage(),
-                start_time: proc.start_time(),
-                mem_usage: proc.memory(),
-                status: proc.status().to_string(),
-            });
-            active = true;
-        } else {
-            active = false;
-        }
-    } else {
-        active = false;
-    }
-
-    if data.view_state != ViewState::Interactable {
-        if data.view_state == ViewState::Visible && !active {
-            data.view_state = ViewState::Hidden;
-            window.hide().unwrap();
-        } else if data.view_state == ViewState::Hidden && active {
-            data.view_state = ViewState::Visible;
-            window.show().unwrap();
-        }
-    }
-    result
 }
 
 #[tauri::command]
@@ -186,11 +165,13 @@ async fn init_login(app: AppHandle) {
 fn overlay_key(window: Window, data: State<Mutex<AppData>>) -> bool {
     let mut data = data.lock().unwrap();
     if data.view_state == ViewState::Interactable {
+        debug!("overlay_key: hiding");
         data.view_state = ViewState::Hidden;
         window.hide().unwrap();
         window.set_ignore_cursor_events(true).unwrap();
         false
     } else {
+        debug!("overlay_key: showing");
         data.view_state = ViewState::Interactable;
         window.show().unwrap();
         window.set_ignore_cursor_events(false).unwrap();
