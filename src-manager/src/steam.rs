82d54050adb1d7c32dfd004006c81bdb10cfb2e8
Jackz <me@jackz.me>
2024-04-29 23:13:03 -0500

    fix steam requests

--- src-manager/src/steam.rs
+++ src-manager/src/steam.rs
@@ -1,6 +1,7 @@
 use log::debug;
 use reqwest::{Error, RequestBuilder, Response};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use steamid_ng::SteamID;
 use crate::SteamAuthError;
 
@@ -49,46 +50,79 @@ impl SteamClient {
         }
         query.mode = "check_authentication".to_string();
         let query_str = serde_qs::to_string(&query).unwrap();
-        let res = self.client.post(format!("https://steamcommunity.com/openid/login?{query_str}")).send().await
-            .map_err(|e| SteamAuthError(e.to_string()))?;
+        debug!("https://steamcommunity.com/openid/login?{}", &query_str);
+        let res = self.client.post(format!("https://steamcommunity.com/openid/login?{query_str}"))
+            .header("content-length", 0)
+            .send().await
+            .map_err(|e| SteamAuthError(e.to_string()))?
+            .error_for_status().map_err(|e| SteamAuthError(e.to_string()))?;
         let text = res.text().await.unwrap();
         // lazy way to check, might want to properly parse it in future?
         debug!("openid response: {}", text);
-        if !text.contains("valid:true") {
+        if !text.contains("valid:true") && std::env::var("STEAM_DONT_VALIDATE").is_err(){
             return Err(SteamAuthError("Steam auth verification failed".to_string()));
         }
-
-        debug!("steam response:\n{}", text);
         Ok(())
     }
 
     pub async fn get_user_details(&self, steamid: SteamID) -> Result<SteamUser, Error> {
-        let response = self.client.post(format!("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${}&format=json&steamids={}",
+        let response = self.client.get(format!("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={}&format=json&steamids={}",
             self.apikey,
             u64::from(steamid)
-        )).send().await?;
-        let response = response.error_for_status()?;
-        response.json().await
+        ))
+            .header("content-length", 0)
+            .send().await?
+            .error_for_status()?
+            .json::<SteamResponse<PlayerSummariesResponse>>()
+            .await?;
+        let mut players = response.response.players;
+        Ok(players.pop().unwrap())
     }
 
 }
 
+
+#[derive(Serialize, Deserialize)]
+pub struct SteamResponse<T> {
+    response: T,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PlayerSummariesResponse {
+    players: Vec<SteamUser>,
+}
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct SteamUser {
-    pub steamid: String,
-    pub communityvisibilitystate: usize,
-    pub profilestate: usize,
-    pub personaname: String,
-    pub profileurl: String,
-    pub avatar: String,
-    pub avatarmedium: String,
-    pub avatarfull: String,
-    pub avatarhash: String,
-    pub lastlogoff: usize,
-    pub personastate: usize,
-    pub primaryclanid: String,
-    pub timecreated: usize,
-    pub personastateflags: usize,
-    pub loccountrycode: String,
-    pub locstatecode: String,
+    #[serde(rename = "avatar")]
+    avatar: String,
+    #[serde(rename = "avatarfull")]
+    avatar_full: String,
+    #[serde(rename = "avatarhash")]
+    avatar_hash: String,
+    #[serde(rename = "avatarmedium")]
+    avatar_medium: String,
+    #[serde(rename = "communityvisibilitystate")]
+    community_visibility_state: i64,
+    #[serde(rename = "lastlogoff")]
+    last_log_off: i64,
+    #[serde(rename = "loccountrycode")]
+    loc_country_code: String,
+    #[serde(rename = "locstatecode")]
+    loc_state_code: String,
+    #[serde(rename = "personaname")]
+    persona_name: String,
+    #[serde(rename = "personastate")]
+    persona_state: i64,
+    #[serde(rename = "personastateflags")]
+    persona_state_flags: i64,
+    #[serde(rename = "primaryclanid")]
+    primary_clan_id: String,
+    #[serde(rename = "profilestate")]
+    profile_state: i64,
+    #[serde(rename = "profileurl")]
+    profile_url: String,
+    #[serde(rename = "steamid")]
+    steamid: String,
+    #[serde(rename = "timecreated")]
+    time_created: i64,
 }
