534573856821bc8c5d4c6d9a88d8e2e6636fc915
Jackz <me@jackz.me>
2025-07-11 19:10:50 -0500

    end ui thread on process lost

--- src-overlay/src/main.rs
+++ src-overlay/src/main.rs
@@ -16,15 +16,17 @@ use std::time::Instant;
 
 use std::str::FromStr;
 use std::sync::{Arc, Mutex, OnceLock};
+use std::sync::atomic::{AtomicBool, Ordering};
+use sysinfo::{ProcessRefreshKind, ProcessesToUpdate, System, UpdateKind};
 use tokio::io::Join;
 use tokio::task::JoinHandle;
-use tracing::debug;
+use tracing::{debug, info};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 use crate::manager::{start_manager_read_thread, OverlayManagerInstance};
 use crate::ui::OverlayData;
 
-const PROCESS_CHECK_INTERVAL: u64 = 500;
+const PROCESS_CHECK_INTERVAL_MS: u64 = 1000;
 
 static TARGET_PROC_NAME: OnceLock<String> = OnceLock::new();
 
@@ -51,23 +53,40 @@ fn main() {
     setup_logging();
 
     // Set up the manager, this sends and receives all requests
-    let target_process = get_target_process();
+    let target_proc_name = get_target_process();
 
     let mut ui_thread: Option<std::thread::JoinHandle<()>> = None;
 
-    debug!("waiting for {}", target_process);
+    let mut sys = System::new();
+
+    debug!("waiting for {}", target_proc_name);
+    let kill_signal = Arc::new(AtomicBool::new(false)); // used to tell UI thread to end
     loop {
-        if true { // process found
+        if check_for_process(&mut sys, &target_proc_name) {
+            // Process active, create ui thread if not already
             if ui_thread.is_none() {
-                debug!("found process, starting UI");
-                ui_thread = Some(create_ui_thread());
+                debug!("found process, spawning UI thread");
+                ui_thread = Some(create_ui_thread(kill_signal.clone()));
             }
+        } else if let Some(thread) = ui_thread.take() {
+            // Process inactive
+            // Kill the ui thread, if set
+            debug!("waiting for UI thread to terminate");
+            kill_signal.store(true, Ordering::Relaxed);
+            thread.join().unwrap();
+            kill_signal.store(false, Ordering::Relaxed);
+            debug!("UI thread terminated");
         }
-        std::thread::sleep(std::time::Duration::from_millis(PROCESS_CHECK_INTERVAL));
+        std::thread::sleep(std::time::Duration::from_millis(PROCESS_CHECK_INTERVAL_MS));
     }
 }
 
-fn create_ui_thread() -> std::thread::JoinHandle<()> {
+fn check_for_process(sys: &mut System, target_name: &str) -> bool {
+    sys.refresh_processes_specifics(ProcessesToUpdate::All, true, ProcessRefreshKind::nothing().with_cmd(UpdateKind::Always));
+    sys.processes_by_name(target_name.as_ref()).any(|p| true)
+}
+
+fn create_ui_thread(kill_signal: Arc<AtomicBool>) -> std::thread::JoinHandle<()> {
     std::thread::spawn(|| {
         let manager = OverlayManagerInstance::new();
         let manager = Arc::new(Mutex::new(manager));
@@ -76,7 +95,8 @@ fn create_ui_thread() -> std::thread::JoinHandle<()> {
         let read_rx = start_manager_read_thread(manager.clone());
 
         // Start the UI
-        let state = OverlayData::example(manager, read_rx);
+        let state = OverlayData::example(manager, read_rx, kill_signal);
+        info!("START ui loop");
         egui_overlay::start(state);
     })
 }
